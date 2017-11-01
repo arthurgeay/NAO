79eae30e13f7fc574f3ebc8dcc3480af28ccbfea
Geteo12 <geteolastpiece@gmail.com>
2017-11-01 15:50:38 +0100

    sécurite et page connexion fonctionnelles + début page inscription

--- src/Omega/NAOBundle/Controller/DefaultController.php
+++ src/Omega/NAOBundle/Controller/DefaultController.php
@@ -3,11 +3,31 @@
 namespace Omega\NAOBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('OmegaNAOBundle:Default:index.html.twig');
+    }
+
+    public function loginAction(Request $request)
+    {
+        // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->redirectToRoute('omega_nao_homepage');
+        }
+
+        // Le service authentication_utils permet de récupérer le nom d'utilisateur
+        // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
+        // (mauvais mot de passe par exemple)
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        return $this->render('OmegaNAOBundle:Default:login.html.twig', array(
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error'         => $authenticationUtils->getLastAuthenticationError(),
+        ));
     }
 }
