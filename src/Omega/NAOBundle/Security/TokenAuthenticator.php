543f5c8527d55ca64526d777fc63c59f365ef699
arthurgeay <arthurgeay.contact@gmail.com>
2017-11-11 02:41:08 +0100

    Intégration google connexion

---
+++ src/Omega/NAOBundle/Security/TokenAuthenticator.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Omega\NAOBundle\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Doctrine\ORM\EntityManager;
+
+class TokenAuthenticator extends AbstractGuardAuthenticator
+{
+    /**
+     * Called on every request. Return whatever credentials you want to
+     * be passed to getUser(). Returning null will cause this authenticator
+     * to be skipped.
+     */
+    
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return $request->query->get('api');
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $user = $this->em->getRepository('OmegaNAOBundle:Utilisateurs')
+            ->findOneBy(array('googleId' => $credentials));
+
+        return $user;
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        if($user->getGoogleId() == $credentials)
+        {
+            return true;
+        }
+
+        throw new MyCustomAuthenticationException('Les identifiants sont incorrects');
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // on success, let the request continue
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $data = array(
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+
+            // or to translate this message
+            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
+        );
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        $data = array(
+            // you might translate this message
+            'message' => 'Authentication Required'
+        );
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
