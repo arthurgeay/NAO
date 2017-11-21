5996bb3ea826ac4df9a5443a758ffe88ce5f6c85
arthurgeay <arthurgeay.contact@gmail.com>
2017-11-21 16:21:15 +0100

    Modifs adresse mail

--- src/Omega/NAOBundle/Services/MailService.php
+++ src/Omega/NAOBundle/Services/MailService.php
@@ -8,13 +8,13 @@ class MailService
     public function getMailService($bodyEmail, $mail, $subject)
     {
         $transport = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
-            ->setUsername('geteolastpiece')
-            ->setPassword('bmpmpnxqojewxlxc');
+            ->setUsername('naoprojet.contact')
+            ->setPassword('_/:7(#?,3Xp7');
         //création d'un objet mailer
         $mailer = \Swift_Mailer::newInstance($transport);
         $message = \Swift_Message::newInstance();
         $message->setSubject($subject);
-        $message->setFrom('geteolastpiece@gmail.com');
+        $message->setFrom('naoprojet.contact@gmail.com');
         $message->setTo($mail);
         // pour envoyer le message en HTML
         $message->setBody($bodyEmail,'text/html');
@@ -25,14 +25,14 @@ class MailService
     public function getMailContactService($bodyEmail, $mail, $subject)
     {
         $transport = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
-            ->setUsername('geteolastpiece')
-            ->setPassword('bmpmpnxqojewxlxc');
+            ->setUsername('naoprojet.contact')
+            ->setPassword('_/:7(#?,3Xp7');
         //création d'un objet mailer
         $mailer = \Swift_Mailer::newInstance($transport);
         $message = \Swift_Message::newInstance();
         $message->setSubject($subject);
         $message->setFrom($mail);
-        $message->setTo('geteolastpiece@gmail.com');
+        $message->setTo('naoprojet.contact@gmail.com');
         // pour envoyer le message en HTML
         $message->setBody($bodyEmail,'text/html');
         //envoi du message
